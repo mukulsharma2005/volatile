bf070c21710f312c14a9dd74a24daa536b071ac8
mukulsharma2005 <mukulsharmamukulsharma23@gmail.com>
2025-10-19 08:29:22 +0530

    Update CORS origin to allow broader access

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ configDotenv()
 
 const app = express();
 const corsOptions = {
-    origin: ["https://mukulsharma2005.github.io/volatile"],
+    origin: ["https://mukulsharma2005.github.io"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies and authentication headers
     optionsSuccessStatus: 204 // For preflight requests
